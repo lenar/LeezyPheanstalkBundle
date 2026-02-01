e8a45aa6f69949f9efff9ea0ed2f6d7dab52c0c0
lenar <lenar@city.ee>
2026-02-01 15:28:12 +0200

    Update LeezyPheanstalkExtension.php

--- src/DependencyInjection/LeezyPheanstalkExtension.php
+++ src/DependencyInjection/LeezyPheanstalkExtension.php
@@ -29,9 +29,9 @@ class LeezyPheanstalkExtension extends Extension
         $configuration = new Configuration();
         $config        = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-        $loader->load('commands.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+        $loader->load('commands.yaml');
 
         $this->configureLocator($container, $config);
         $this->configureLogListener($container, $config);
